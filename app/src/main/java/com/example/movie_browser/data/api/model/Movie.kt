f8ffd47b7db6355930c5ff28cc876fef2598fc68
IASPERMOGLOT <bulatooov@gmail.com>
2026-03-20 23:22:12 +0300

    add data classes: Movie and MovieResponse

---
+++ app/src/main/java/com/example/movie_browser/data/api/model/Movie.kt
@@ -0,0 +1,37 @@
+package com.example.movie_browser.data.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Movie(
+    val adult: Boolean,
+    @SerialName("backdrop_path") val backdropPath: String,
+    @SerialName("genre_ids") val genreIds: List<Int>,
+    val id: Int,
+    @SerialName("original_language") val originalLanguage: String,
+    @SerialName("original_title") val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    @SerialName("poster_path") val posterPath: String,
+    @SerialName("release_date") val releaseDate: String,
+    val title: String,
+    val video: Boolean,
+    @SerialName("vote_average") val voteAverage: Double,
+    @SerialName("vote_count") val voteCount: Int
+)
+
+//      "adult": false,
+//      "backdrop_path": "/6yeVcxFR0j08vlv2OlL6zbewm4D.jpg",
+//      "genre_ids": [28, 878, 53],
+//      "id": 1265609,
+//      "original_language": "en",
+//      "original_title": "War Machine",
+//      "overview": "Во время последней изнурительной миссии на подготовке рейнджеров боевой инженер должен возглавить подразделение в схватке с гигантской потусторонней машиной-убийцей.",
+//      "popularity": 377.835,
+//      "poster_path": "/eB863opfwAn0kV6SSWkfVUEzs2n.jpg",
+//      "release_date": "2026-02-12",
+//      "title": "Военная машина",
+//      "video": false,
+//      "vote_average": 7.273,
+//      "vote_count": 983
---
+++ app/src/main/java/com/example/movie_browser/data/api/model/MovieResponse.kt
@@ -0,0 +1,12 @@
+package com.example.movie_browser.data.api.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieResponse(
+    val page: Int,
+    val results: List<Movie>,
+    @SerialName("total_pages") val totalPages: Int,
+    @SerialName("total_results") val totalResults: Int
+)
